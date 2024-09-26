459e4df1f6d10f7357e19659de17fc004eb97829
test <test@test.com>
2024-09-26 01:25:42 -0700

    actually fixing allowed hosts

--- game_server/settings.py
+++ game_server/settings.py
@@ -26,8 +26,6 @@ SECRET_KEY = 'django-insecure-nq6@7#mw2^wdcg#8b%_%sq)7b^mh7-ikk#tx)0hv%2!(gs1wp+
 # CORS
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
-    'http://167.71.124.163',
-    'http://167.71.124.163:8000',
     'http://localhost:8000'
 ]
 CORS_ALLOWED_ORIGIN_REGEXES = [
@@ -38,7 +36,9 @@ CORS_ALLOW_CREDENTIALS = True  # For cookies or HTTP authentication
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'http://167.71.124.163',
+]
 
 
 # Application definition
